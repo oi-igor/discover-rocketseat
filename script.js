4ad9dc0b4f20c27fcb148a19619d4d61b198d608
Igor Alexandrenotepad <97889222+oi-igor@users.noreply.github.com>
2023-09-27 13:41:47 -0300

    add readme

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function toggleMode(){
 
     const img = document.querySelector('#profile img')
 
-
     if(html.classList.contains('light')){
         img.setAttribute('src', './assets/avatar-light.png')
         img.setAttribute('alt', 'Foto em formato redondo com bordas pretas de Igor Alexandre sorrindo, de camisa azul escuro com paisagens de arvores ao fundo.')
@@ -12,5 +11,4 @@ function toggleMode(){
         img.setAttribute('src', './assets/avatar.png')
         img.setAttribute('alt', 'Foto em formato redondo com bordas brancas de Igor Alexandre sorrindo, de camisa azul escuro com paisagens de arvores ao fundo.')
     }
-
 }
